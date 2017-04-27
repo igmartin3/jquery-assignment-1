511dbdf340b672e186e8e5f17a98c57eaaffa2da
Ignacio Martinez <ignaciomartinez@dhcp64-131-120-178.kentlaw.edu>
2017-04-27 12:59:16 -0500

    Initial Commit adding divs and click event

--- part-2/script.js
+++ part-2/script.js
@@ -1,1 +1,25 @@
 // Do your work in this file.
+for (i = 1; i < 197; i++) {
+    var boxDiv = document.createElement("div");
+    boxDiv.className = "box";
+
+    //var type1Div = document.createElement("div");
+    //var type2Div = document.createElement("div");
+
+    if ((i % 2) == 0) {
+        $(boxDiv).addClass("type-1");
+    } else {
+        $(boxDiv).addClass("type-2");
+    }
+
+    $(boxDiv).appendTo($(document.body));
+}
+
+
+$(".box").click(function() {
+    if ( $(this).hasClass("clicked")){
+        $(this).removeClass("clicked");
+    } else {
+         $(this).addClass("clicked");
+    }
+});
