948f541636e86471cdb5c3b3412a62c3dff26aee
Ignacio Martinez <ignaciomartinez@Ignacios-MacBook-Pro.local>
2017-04-28 08:38:35 -0500

    Updated all Javascript to create divs and assign classes to Jquery

--- part-1/script.js
+++ part-1/script.js
@@ -1,46 +1,65 @@
 // Do your work in this file.
-var viewDiv = document.createElement("div");
-viewDiv.className = "view";
 
-$( viewDiv ).appendTo(document.body);
 
-var grassDiv = document.createElement("div");
-grassDiv.className = "grass";
+//var viewDiv = document.createElement("div");
+//viewDiv.className = "view";
 
-$( grassDiv ).appendTo( $(viewDiv) );
+var viewDiv = $("<div class='view'></div>").appendTo("body");
 
-var sunDiv = document.createElement("div");
-sunDiv.className = "sun";
+//$( viewDiv ).appendTo(document.body);
 
-$(sunDiv).appendTo($(viewDiv));
+//var grassDiv = document.createElement("div");
+//grassDiv.className = "grass";
+
+//$( grassDiv ).appendTo( $(viewDiv) );
+
+
+var grassDiv = $("<div class='grass'></div>").appendTo(viewDiv);
+
+//var sunDiv = document.createElement("div");
+//sunDiv.className = "sun";
+
+//$(sunDiv).appendTo($(viewDiv));
+
+var sunDiv = $("<div class='sun'></div>").appendTo(viewDiv);
 
 var i = 5;
 while (i > 0) {
-    var rayDiv = document.createElement("div");
-    rayDiv.className = "ray-"+i;
+    //var rayDiv = document.createElement("div");
+    //rayDiv.className = "ray-"+i;
 
-    $(rayDiv).appendTo($(sunDiv));
+    //$(rayDiv).appendTo($(sunDiv));
+
+    var rayDiv = $("<div></div>").appendTo(sunDiv);
+    rayDiv.addClass("ray-"+i);
     i--;
 }
 
-var mountainDiv = document.createElement("div");
-mountainDiv.className = "mountain";
+//var mountainDiv = document.createElement("div");
+//mountainDiv.className = "mountain";
 
-$(mountainDiv).appendTo($(viewDiv));
+//$(mountainDiv).appendTo($(viewDiv));
 
-var mountainTopDiv = document.createElement("div");
-mountainTopDiv.className = "mountain-top";
+var mountainDiv = $("<div class='mountain'></div>").appendTo(viewDiv);
 
-$(mountainTopDiv).appendTo($(mountainDiv));
+
+//var mountainTopDiv = document.createElement("div");
+//mountainTopDiv.className = "mountain-top";
+
+//$(mountainTopDiv).appendTo($(mountainDiv));
+
+var mountainTopDiv = $("<div class='mountain-top'></div>").appendTo(mountainDiv);
 
 var x = 1;
 
 do {
-    var mountainCapDiv = document.createElement("div");
-    mountainCapDiv.className = "mountain-cap-"+x;
+    //var mountainCapDiv = document.createElement("div");
+    //mountainCapDiv.className = "mountain-cap-"+x;
 
-    $(mountainCapDiv).appendTo($(mountainTopDiv));
+    //$(mountainCapDiv).appendTo($(mountainTopDiv));
 
+    var mountainCapDiv = $("<div></div>").appendTo(mountainTopDiv);
+    mountainCapDiv.addClass("mountain-cap-"+x);
     x++;
 
 } while (x < 4);
